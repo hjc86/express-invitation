a496e8407c59bb70b7278912f32560aae83bb53b
Helder Carmen <heldercarmen@gmail.com>
2020-03-27 15:32:30 +0000

    add pug templates to render dynamically

--- app.js
+++ app.js
@@ -11,24 +11,31 @@ app.use(express.static('views'));
 // app.use(express.bodyParser());
 app.get("/form", (req, res, next) => { 
         res.sendFile(path.join(__dirname+'/views/form.html'));
-
 });
+app.set('view engine', 'pug');
 
 app.post("/invitation-request", (req,res,next)=> {
     
     
-    console.log(req.body);
-    res.send(req.body);
-    // let greeting = req.body.greeting;
-    // let event = req.body.event;
-    // let text = req.body.text;
+    // console.log(req.body);
+    // res.send(req.body);
+    let greeting = req.body.greeting;
+    let event = req.body.event;
+    let text = req.body.text;
+
     
-    // let unsplash = `https://source.unsplash.com/500x500/?${event}`;
+    let unsplash_url = `https://source.unsplash.com/500x500/?${event}`;
+
 
     // res.send(`<h1> ${greeting}   </h1> 
     //            <img src=${unsplash}>         
+    //         `);
 
-    //             `);
+
+    res.render('invitation', { title: greeting, message: text, eventImage: unsplash_url})
+
+    // create html repsonse file in our views using esj templating 
+    
 
     // res.sendfile(path.join(__dirname+'/views/invitation.html'));
     
