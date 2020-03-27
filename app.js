d85780126c4f72520b4f486bf8114f5947fdc786
Helder Carmen <heldercarmen@gmail.com>
2020-03-27 21:34:05 +0000

    add loading page

--- app.js
+++ app.js
@@ -1,47 +1,90 @@
-const express = require("express");
+const express = require('express');
 const path = require('path');
 const router = express.Router();
 const app = express();
+const https = require('https');
 
 const PORT = process.env.PORT || 4001;
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.static('public'));
 app.use(express.static('views'));
-// app.use(express.bodyParser());
-app.get("/form", (req, res, next) => { 
-        res.sendFile(path.join(__dirname+'/views/form.html'));
-});
 app.set('view engine', 'pug');
 
-app.post("/invitation-request", (req,res,next)=> {
-    
-    
-    // console.log(req.body);
-    // res.send(req.body);
+// app.use(express.bodyParser());
+
+
+// app.get("/", function(httpRequest, httpResponse, next){
+//     httpResponse.write("Hello");
+//     next(); //remove this and see what happens 
+// });
+
+// app.get("/", function(httpRequest, httpResponse, next){
+//     httpResponse.write(" World !!!");
+//     httpResponse.end();
+// });
+
+// app.listen(PORT);
+
+
+
+app.get("/", (req, res, next) => { 
+        res.sendFile(path.join(__dirname+'/views/form.html'));
+});
+
+const jj = app.post("/invitation-request", (req,res,next)=> {
     let greeting = req.body.greeting;
     let event = req.body.event;
     let text = req.body.text;
 
-    
-    let unsplash_url = `https://source.unsplash.com/500x500/?${event}`;
+    const options = {
+        hostname: 'source.unsplash.com',
+        path: `/500x500/?${event}`,
+        method: 'GET'
+    }
 
+    function getUnsplashUrl(options){
+        return new Promise((resolve) => {
+            let HTTPreq = https.request(options, result =>{
+                result.setEncoding('utf8');
+                let _header = result.headers;
+                let unsplashUrl=_header['location'];
+                resolve(unsplashUrl);
+            });
+            HTTPreq.end();
+        })
+    }
 
-    // res.send(`<h1> ${greeting}   </h1> 
-    //            <img src=${unsplash}>         
-    //         `);
-
-
-    res.render('invitation', { title: greeting, message: text, eventImage: unsplash_url})
-
-    // create html repsonse file in our views using esj templating 
-    
-
-    // res.sendfile(path.join(__dirname+'/views/invitation.html'));
-    
+    getUnsplashUrl(options).
+        then(result=>{
+            console.log("This is the url for photo", result);
+            res.redirect('/loading');
+            //res.sendFile(path.join(__dirname+'/views/loading.html'));         
+            // res.end()
+            return result
+        })
+        .then(result1=>{
+            showInvite(greeting,text,event,result1)});
+    return "aaaba"
 });
+
+console.log(jj)
+
+
+app.get("/loading", (req,res,next)=> {
+    res.write("loading......")   
     
+    // res.end()
+});
+
+function showInvite(greeting,text,event,result){
+//    res.render('invitation', { title: greeting, message: text, eventImage: result})   
+    res.write("invite should be here")
+    res.end()
+}
+
 
 app.listen(PORT, () => {
     console.log(`listening on port: ${PORT}`);
 })
+
